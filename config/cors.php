fb047a7ad00ac2510b52e4121f251f173365e80a
samialsohan <samialsohan@gmail.com>
2025-11-07 15:12:17 +0000

    cors.php

--- config/cors.php
+++ config/cors.php
@@ -1,20 +1,11 @@
 <?php
 return [
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '/register', '/login'],
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => explode(',', env('FRONTEND_URL', 'https://ezdokani.com,https://www.ezdokani.com')),
-
+    'allowed_origins' => ['https://ezdokani.com', 'https://www.ezdokani.com'],
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => true, // ✅ required for cross-domain cookies
-
+    'supports_credentials' => true,
 ];
